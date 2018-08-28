1e8ddcc58a7d3320a7874089968af6e0f1e38000
vvictorious <vvictorious30@gmail.com>
2018-08-28 16:19:43 -0600

    consoling the input is working

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-//listen for form submit
+// listen for form submit
 document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+	e.preventDefault();
+
+//Get values
+var name = getInputVal('name');
+var name = getInputVal('name');
+var name = getInputVal('name');
+var name = getInputVal('name');
+var name = getInputVal('name');
+}
+
+//function to get form values
+function getInputVal(){
+	return document.getElementById(id).value;
+}
