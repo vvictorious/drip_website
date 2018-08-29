c7a7ee80829271228e51e7c835ced2a8ec9a8172
vvictorious <vvictorious30@gmail.com>
2018-08-29 11:14:09 -0600

    data is going into firebase

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCBsnqJxdsJi3VMYrNhxEQ5FaANtZ5VHTs",
+    authDomain: "drip-contactform.firebaseapp.com",
+    databaseURL: "https://drip-contactform.firebaseio.com",
+    projectId: "drip-contactform",
+    storageBucket: "drip-contactform.appspot.com",
+    messagingSenderId: "247014451177"
+  };
+  firebase.initializeApp(config);
+
+//reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
 // listen for form submit
 document.getElementById('contactForm').addEventListener('submit', submitForm);
 
+//submit form
 function submitForm(e){
 	e.preventDefault();
 
@@ -10,7 +25,9 @@ var company = getInputVal('company');
 var email = getInputVal('email');
 var phone = getInputVal('phone');
 var message = getInputVal('message');
-console.log(name);
+
+//save message
+saveMessage(name, company, email, phone, message);
 }
 
 //function to get form values
@@ -18,5 +35,15 @@ function getInputVal(id){
 	return document.getElementById(id).value;
 }
 
-
+//save message to firebase
+function saveMessage(name, company, email, phone, message){
+	var newMessageRef = messagesRef.push();
+	newMessageRef.set({
+		name: name,
+		company: company,
+		email: email,
+		phone: phone, 
+		message: message
+	});
+}
 
