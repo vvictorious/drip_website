01c35db54efb8156be7e7a4744688b175b4ee981
vvictorious <vvictorious30@gmail.com>
2018-08-30 12:31:24 -0600

    happy for now

--- main.js
+++ main.js
@@ -18,16 +18,22 @@ document.getElementById('contactForm').addEventListener('submit', submitForm);
 //submit form
 function submitForm(e){
 	e.preventDefault();
-
-//Get values
-var name = getInputVal('name');
-var company = getInputVal('company');
-var email = getInputVal('email');
-var phone = getInputVal('phone');
-var message = getInputVal('message');
-
-//save message
-saveMessage(name, company, email, phone, message);
+	//Get values
+	var name = getInputVal('name');
+	var company = getInputVal('company');
+	var email = getInputVal('email');
+	var phone = getInputVal('phone');
+	var message = getInputVal('message');
+	//save message
+	saveMessage(name, company, email, phone, message);
+	//show alert
+	document.querySelector('.alert').style.display = 'block';
+	//hide alert after 3 seconds
+	setTimeout(function(){
+		document.querySelector('.alert').style.display = 'none';
+	},3000);
+	//clear form
+	document.getElementById('contactForm').reset();
 }
 
 //function to get form values
